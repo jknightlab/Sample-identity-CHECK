1b427ea2be9d7e708305840c615b75a4d026d0fa
PingGENO <53820716+PingGENO@users.noreply.github.com>
2020-10-29 17:31:01 +0000

    Add files via upload

---
+++ CrossCheck.R
@@ -0,0 +1,31 @@
+library(pheatmap)
+library(data.table)
+
+data <- as.data.frame(fread("~/sample.140.crosscheck_LOD_Matrix_MAPQ255.txt", stringsAsFactors = F))
+
+# low mapping rate sample removal                             
+data$BIONIC_133_preop_rep = NULL
+data = data[data$READGROUP !="BIONIC_133_preop_rep",]
+
+# 
+row.names(data) <- data$READGROUP
+data <- data[,-1]
+
+# str(data) to check the if the valules are numerics 
+# if yes, replace the characters/factors as numerics NOTE: replace the "," to "" to avoid generated incorrected NA values
+data[] <- lapply(data, function(x) as.numeric(as.character(gsub(",","",x))))
+
+# order the columns/rows by names
+data <- data[, order(colnames(data))]
+data <- data[order(rownames(data)), ]
+
+# generate matrix and plot by pheatmap
+data_matrix <- data.matrix(data)
+data_matrix[data_matrix > 200] <- 200
+data_matrix[data_matrix < -200] <- -200
+#
+pheatmap(data_matrix, col =colorRampPalette(brewer.pal(4, "PRGn"))(10),
+         cluster_rows = F,cluster_cols = F)  #
+
+
+sessionInfo()
