b43551b7ae7deb43e148a25837615bd21fa8cad7
PingGENO <53820716+PingGENO@users.noreply.github.com>
2022-11-02 14:17:52 +0000

    Update CrossCheck.R

--- CrossCheck.R
+++ CrossCheck.R
@@ -1,26 +1,20 @@
-library(data.table)
-library(RColorBrewer)
 library(pheatmap)
-
-data <- as.data.frame(fread("~/sample.140.crosscheck_LOD_Matrix_MAPQ255.txt", stringsAsFactors = F))
-
-# low mapping rate sample removal                             
-data$BIONIC_133_preop_rep = NULL
-data = data[data$READGROUP !="BIONIC_133_preop_rep",]
+library(data.table)
+data <- as.data.frame(fread("~/test_LOD_Matrix_MAPQ255.txt", stringsAsFactors = F))
 
 # 
 row.names(data) <- data$READGROUP
 data <- data[,-1]
 
 # str(data) to check the if the valules are numerics 
-# if no, replace the characters/factors as numerics NOTE: replace the "," to "" to avoid generating incorrected NA values
+# if yes, replace the characters/factors as numerics NOTE: replace the "," to "" to avoid generated incorrected NA values
 data[] <- lapply(data, function(x) as.numeric(as.character(gsub(",","",x))))
 
 # order the columns/rows by names
 data <- data[, order(colnames(data))]
 data <- data[order(rownames(data)), ]
 
-# generate matrix and plot by pheatmap
+# generate matrix and plot using pheatmap
 data_matrix <- data.matrix(data)
 data_matrix[data_matrix > 200] <- 200
 data_matrix[data_matrix < -200] <- -200
